c20da3c0223099462a83a18117d60ddeaab2608c
Logan T. Meyer <logan.t.meyer@gmail.com>
2024-10-03 09:50:31 -0600

    v.1

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@
 # ./deploy.sh
 
 
-STACK_NAME="s3-endpoint-monitoring"
-S3_BUCKET_NAME="observability-synthetic-canaries"
+STACK_NAME="observability-synthetic-canaries"
 REGION="us-east-1"  # or any region where you want to deploy the canary
 
 aws cloudformation deploy \
     --template-file template.yaml \
     --stack-name $STACK_NAME \
-    --parameter-overrides S3BucketName=$S3_BUCKET_NAME \
     --capabilities CAPABILITY_IAM \
     --region $REGION
